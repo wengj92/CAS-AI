546e7d2c80d9b3144ef300aa40fd1c4846e5ee92
wengj92 <joel.wenger@hotmail.com>
2023-01-17 16:06:03 +0100

    added exercise 3 (transfer learning)

--- Exercises/week_03/plots.py
+++ Exercises/week_03/plots.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
 import torchvision
 
 
@@ -8,6 +9,30 @@ def show_images(images):
     img = img / 2 + 0.5
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+def visualize_model(model, dataloaders, class_names, device='cpu', num_images=6):
+    was_training = model.training
+    model.eval()
+    images_so_far = 0
+    fig = plt.figure()
+    with torch.no_grad():
+        for i, (inputs, labels) in enumerate(dataloaders['val']):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            for j in range(inputs.size()[0]):
+                images_so_far += 1
+                ax = plt.subplot(num_images//2, 2, images_so_far)
+                ax.axis('off')
+                ax.set_title(f'predicted: {class_names[preds[j]]}')
+                show_images(inputs.cpu().data[j])
+                if images_so_far == num_images:
+                    model.train(mode=was_training)
+                    return
+        model.train(mode=was_training)
     plt.show()
 
 
